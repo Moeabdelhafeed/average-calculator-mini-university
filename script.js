4e4dcfde044ae949962d265e01e3fb63b7e4f6cf
Mohammadabdelhafeed <almormohammad939@gmail.com>
2024-07-07 04:37:13 +0300

    first version

---
+++ script.js
@@ -0,0 +1,20 @@
+function showresult(){
+
+
+    var result = document.getElementById("result");
+    result.value = "average is: "
+
+    let sum = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let inputValue = document.getElementById("input_" +i).value;
+        if (inputValue == ""){
+            alert("you should fill out all the feilds");
+            return 1;
+        }
+        sum += parseInt(inputValue); 
+    }
+    
+
+    result.value += (sum/5);
+}
